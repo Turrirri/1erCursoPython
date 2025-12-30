481e1c2351afc9f51697e89430e049cf5c9062dd
Turriri <picure01@gmail.com>
2025-12-29 22:13:42 -0300

    2 parte del curso de python

---
+++ tipos de datos/datos-simples.py
@@ -0,0 +1,19 @@
+"string"
+
+"string"
+
+"""tus datos son:
+    nombre: lucas
+    apellido: dalto"""
+
+"""tuss datos son:
+    nombre: lucas
+    apellido: dalto"""
+
+
+40
+
+40.2
+
+False
+True
---
+++ variables/variables.py
@@ -0,0 +1,14 @@
+a = 15
+
+b = 3
+c = a + b
+
+print(c)
+
+nombre = "Jhonny"
+bienvenida = f"Hola {nombre} ¿Cómo estás?"
+print(bienvenida)
+
+# operadores de pertenecia ('in' y 'not in)
+print("hon" in bienvenida)
+print("Lucas" not in bienvenida)
