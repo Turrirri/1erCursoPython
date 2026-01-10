5e4cb16b7328385a1b711aea5ec7a9401871fd4d
Turriri <picure01@gmail.com>
2026-01-10 08:58:13 -0300

    modificado variables.py

--- variables/variables.py
+++ variables/variables.py
@@ -5,10 +5,17 @@ c = a + b
 
 print(c)
 
-nombre = "Jhonny"
+nombre = 'Jhonny'
+
+# concatenar con +
+bienvenida = "Hola " + nombre + " ¿Cómo estas?"  ''' Al concatenar con '+' hay\
+que estar pendiente de dejar espacios '''
+print(bienvenida)
+
+# concatenar con f-strings
 bienvenida = f"Hola {nombre} ¿Cómo estás?"
 print(bienvenida)
 
 # operadores de pertenecia (in / not in)
 print("nny" in bienvenida)
-print("Lucas" not in bienvenida)
+print("Jhonny" not in bienvenida)
